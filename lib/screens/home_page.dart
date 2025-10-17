eac994b3ac71e03d008b911bc2a48bdcc430f15f
BBoezio <benjamin.boezio@labom2iformation.fr>
2025-10-17 13:09:56 +0200

    make HomePage dynamic with footer navigation

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,8 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:petit_chef/widgets/partials/footer.dart';
 import 'package:petit_chef/widgets/partials/header.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String activePage = 'Home';
+
+  void onFooterItemSelected(String label) {
+    setState(() {
+      activePage = label;
+    });
+  }
+
+  Widget _buildPageContent() {
+    switch (activePage) {
+      case 'Home': return const Text('Bienvenue sur HomePage');
+      case 'Search': return const Text('Rechercher une recette');
+      case 'List': return const Text('Votre liste');
+      case 'Profile': return const Text('Votre profil');
+      default: return const Text('Bienvenue sur la HomePage');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +38,12 @@ class HomePage extends StatelessWidget {
           Header(),
 
           Expanded(
-            child: SingleChildScrollView(child: Column(children: [])),
+            child: SingleChildScrollView(
+                child: _buildPageContent()
+            ),
           ),
 
-          Footer(),
+          Footer(onItemSelected: onFooterItemSelected, activeLabel: activePage),
         ],
       ),
     );

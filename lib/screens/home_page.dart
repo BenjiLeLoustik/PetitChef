d65ed37032c61ecafc77a09e4657a7aec832a194
BBoezio <benjamin.boezio@labom2iformation.fr>
2025-10-17 11:10:24 +0200

    Update HomePage layout with header, content and footer sections

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,7 +6,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Text('HomePage')
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+          Container(
+            color: Colors.red,
+            height: 60,
+          ),
+
+          Expanded(
+            child: Container(
+              color: Colors.blue
+            ),
+          ),
+
+          Container(
+            color: Colors.green,
+            height: 70,
+          ),
+
+        ],
+      )
     );
   }
 }

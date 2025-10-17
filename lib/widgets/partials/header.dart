88d733181b56752e45dc00c37206e1c1793d7b1d
BBoezio <benjamin.boezio@labom2iformation.fr>
2025-10-17 11:36:51 +0200

    Replace red placeholder with logo image in Header

--- lib/widgets/partials/header.dart
+++ lib/widgets/partials/header.dart
@@ -5,13 +5,21 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Container(
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(height: 50, width: 50, color: Colors.red),
+          Container(
+            height: 50,
+            width: 60,
+            alignment: Alignment.center,
+            child: Image.asset(
+              'images/petitchef_logo.png',
+              fit: BoxFit.contain,
+            ),
+          ),
           SizedBox(width: 20),
           Expanded(child: Container(height: 50, width: 50, color: Colors.red)),
         ],

c99c68139e1a1eb4e00dca8598ccdc8f5fc3759e
BBoezio <benjamin.boezio@labom2iformation.fr>
2025-10-17 12:47:35 +0200

    Refactor Footer with reusable FooterButton widget and add hover animations

--- lib/widgets/partials/footer.dart
+++ lib/widgets/partials/footer.dart
@@ -11,64 +11,69 @@ class Footer extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.home_rounded, color: Color(0xFFD8D8D8)),
-                SizedBox(height: 5),
-                Text(
-                  'Home',
-                  style: TextStyle(color: Color(0xFFD8D8D8), fontSize: 14),
-                ),
-              ],
-            ),
-          ),
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.search, color: Color(0xFFD8D8D8)),
-                SizedBox(height: 5),
-                Text(
-                  'Recherche',
-                  style: TextStyle(color: Color(0xFFD8D8D8), fontSize: 14),
-                ),
-              ],
-            ),
-          ),
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.shopping_cart, color: Color(0xFFD8D8D8)),
-                SizedBox(height: 5),
-                Text(
-                  'Liste',
-                  style: TextStyle(color: Color(0xFFD8D8D8), fontSize: 14),
-                ),
-              ],
-            ),
-          ),
-          InkWell(
-            onTap: () {},
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.account_circle, color: Color(0xFFD8D8D8)),
-                SizedBox(height: 5),
-                Text(
-                  'Profil',
-                  style: TextStyle(color: Color(0xFFD8D8D8), fontSize: 14),
-                ),
-              ],
-            ),
-          ),
+          FooterButton(label: 'Home', icon: Icons.home_rounded),
+          FooterButton(label: 'Recherche', icon: Icons.search),
+          FooterButton(label: 'Liste', icon: Icons.shopping_cart),
+          FooterButton(label: 'Profil', icon: Icons.account_circle),
         ],
       ),
+    );
+  }
+}
+
+class FooterButton extends StatefulWidget {
+  final IconData icon;
+  final String label;
+
+  const FooterButton({
+    super.key,
+    required this.label,
+    required this.icon
+  });
+
+  @override
+  State<FooterButton> createState() => _FooterButtonState();
+}
+
+class _FooterButtonState extends State<FooterButton> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    const duration = Duration(milliseconds: 200);
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => isHovered = true),
+      onExit: (_) => setState(() => isHovered = false),
+      child: InkWell(
+        onTap: (){},
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TweenAnimationBuilder(
+              tween: ColorTween(
+                begin: Color(0xFFD8D8D8),
+                end: isHovered ? Colors.orange : Color(0xFFD8D8D8),
+              ),
+              duration: duration,
+              builder: (context, color, child) {
+                return Icon(widget.icon, color: color);
+              },
+            ),
+            SizedBox(height: 5),
+            AnimatedDefaultTextStyle(
+              duration: duration,
+              style: TextStyle(
+                  color: isHovered ? Color(0xFF2E2E2E) : Color(0xFFD8D8D8),
+                  fontSize: 14
+              ),
+              child: Text(
+                widget.label
+              )
+            ),
+          ],
+        )
+      )
     );
   }
 }
